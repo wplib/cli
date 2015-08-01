4c4f20c386fa23d48ab1596c6e6918849b342d1f
Mike Schinkel <mike@newclarity.net>
2015-07-31 20:48:29 -0400

    Registered a wplib_dir to ensure it is created.

--- generators/root-generator.php
+++ generators/root-generator.php
@@ -7,18 +7,35 @@ namespace WPLib_CLI {
 	 *
 	 * @package WPLib_CLI
 	 *
+	 * @property string $wplib_dir
 	 */
 	class Root_Generator extends Generator {
 
 		const SLUG = 'root';
 
+
+
 		function register() {
 
 			parent::register();
+			/**
+			 * @todo Add some way to checkout WPLib to this dir, if not there.
+			 */
+			$this->register_dirs( $this->wplib_dir );
 
 			$this->register_generator( Theme::SLUG, $this->theme() );
 
 			$this->register_generator( App::SLUG, $this->app() );
+
+		}
+
+
+		/**
+		 * @return string
+		 */
+		function wplib_dir() {
+
+			return "{$this->theme->theme_dir}/wplib";
 
 		}
 
